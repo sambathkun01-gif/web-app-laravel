9b6751c9708b1b933007cce9793e28f4bed1acb1
sambath <sambathkun01@gmail.com>
2026-05-15 10:28:06 +0700

    Route

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::get('/users', function () {
+return ['name' => 'John', 'email' => 'john@example.com'];
+});

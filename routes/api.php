db3fda400db7637d703a6009974e355f5a0f52eb
sambath <sambathkun01@gmail.com>
2026-06-24 15:22:30 +0700

    api-posts

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\PostController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -9,3 +10,8 @@ Route::get('/user', function (Request $request) {
 Route::get('/users', function () {
 return ['name' => 'John', 'email' => 'john@example.com'];
 });
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
